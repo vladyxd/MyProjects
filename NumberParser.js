3caafa3959dbc9e4d443f718fec1ce1a054c77c9
VLADY <41295778+vladyxd@users.noreply.github.com>
2023-01-06 16:11:03 +0200

    Updated NumberParser.js

--- NumberParser.js
+++ NumberParser.js
@@ -3,7 +3,6 @@ function fetchNumber(number){
 if(!number) number = this;
 let string;
 if(number / 1000000000000 < 1){
-
 if(number / 1000000000 < 1){
 if(number / 1000000 < 1){if(number / 1000 > 1) string = `${number / 1000}K`; else return string = number.toString()}
 else string = `${number / 1000000}M`}
@@ -13,3 +12,4 @@ else string = `${number/1000000000000}Q`
 return string;
 }
 Number.prototype.vlady = fetchNumber;
+// console.log(Number(1200000).vlady()) -> 1.2M
